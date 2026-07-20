f31769297bab27779c72a105f7b91cc11d87a6cc
Miguel_Reis <79938486+MiguelReisB@users.noreply.github.com>
2026-07-20 19:38:46 -0300

    adicionado toda a filmografia do diretor de cinema Christopher Nolan

--- main.py
+++ main.py
@@ -4,17 +4,46 @@ import interface
 
 filmes = [
     {
-        "nome": "A Origem",
-        "genero": "Ficção Científica",
-        "atores_principais": ["Leonardo DiCaprio", "Tom Hardy", "Cillian Murphy"],
-        "duracao":148
+        "nome": "Following",
+        "genero": "Thriller",
+        "atores_principais": ["Jeremy Theobald", "John Nolan", "Lucy Thackeray"],
+        "duracao":69
     },
-
+    {
+        "nome": "Memento",
+        "genero": "Thriller",
+        "atores_principais": ["Guy Pearce", "Carrie-Anne Moss", "Joe Pantoliano"],
+        "duracao":113
+    },
+    {
+        "nome": "Insônia",
+        "genero": "Thriller",
+        "atores_principais": ["Al Pacino", "Robin Williams", "Hilary Swank"],
+        "duracao":118
+    },
     {
         "nome": "Batman Begins",
         "genero": "Ação",
         "atores_principais": ["Christian Bale", "Cillian Murphy", "Liam Neeson"],
         "duracao":140
+    },
+    {
+        "nome": "O Grande Truque",
+        "genero": "Thriller",
+        "atores_principais": ["Christian Bale", "Hugh Jackman", "Scarlett Johansson"],
+        "duracao":130
+    },
+    {
+        "nome": "Batman: O Cavaleiro das Trevas",
+        "genero": "Ação",
+        "atores_principais": ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+        "duracao":152
+    },
+    {
+        "nome": "A Origem",
+        "genero": "Ficção Científica",
+        "atores_principais": ["Leonardo DiCaprio", "Tom Hardy", "Cillian Murphy"],
+        "duracao":148
     },
     {
         "nome": "Batman: O Cavaleiro das Trevas Ressurge",
@@ -29,10 +58,28 @@ filmes = [
         "duracao":169
     },
     {
+        "nome": "Dunkirk",
+        "genero": "Guerra",
+        "atores_principais": ["Tom Hardy", "Cillian Murphy", "Kenneth Branagh"],
+        "duracao":106
+    },
+    {
+        "nome": "Tenet",
+        "genero": "Ficção Científica",
+        "atores_principais": ["John David Washington", "Robert Pattinson", "Elizabeth Debicki"],
+        "duracao":150
+    },
+    {
         "nome": "Oppenheimer",
         "genero": "Drama",
         "atores_principais": ["Cillian Murphy", "Robert Downey Jr.", "Matt Damon"],
         "duracao":180
+    },
+    {
+        "nome": "A Odisseia",
+        "genero": "Fantasia",
+        "atores_principais": ["Matt Damon", "Tom Holland", "Anne Hathaway"],
+        "duracao":173
     }
 ]
 
